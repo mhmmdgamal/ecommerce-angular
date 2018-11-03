dd910c10de545b04c623ea2ac375998cd7fa19a4
Mohamed Gamal <mohamed.gamal987.mg@gmail.com>
2018-11-03 18:41:07 +0200

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,71 @@
+// modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+// components
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserAddComponent } from './user-add/user-add.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
+
+// services
+import { UserService } from './shared/user.service';
+
+
+
+const appRoutes: Routes = [
+    {
+      path: '',
+      redirectTo: '/home',
+      pathMatch: 'full'
+    },
+    {
+      path: 'home',
+      component: HomeComponent
+    },
+    {
+      path: 'user-list',
+      component: UserListComponent
+    },
+    {
+      path: 'user-add',
+      component: UserAddComponent
+    },
+    {
+      path: 'user-edit/:id',
+      component: UserEditComponent
+    }
+  ];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      HomeComponent,
+      UserListComponent,
+      UserAddComponent,
+      UserEditComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule,
+      MatButtonModule,
+      MatCardModule,
+      MatInputModule,
+      MatListModule,
+      MatToolbarModule,
+      FormsModule,
+      RouterModule.forRoot(appRoutes)
+   ],
+   providers: [
+      UserService
+   ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
---
+++ src/app/shared/user.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private api  = '//localhost:8080';
+  private userApi = this.api + '/users';
+
+  constructor(private http: HttpClient) { }
+
+  getall(): Observable<any> {
+    // return this.http.get(this.userApi + '/list');
+    return this.http.get('https://randomuser.me/api/?results=5');
+  }
+
+  get(id: string) {
+    // return this.http.get(this.userApi + '/' + id);
+  }
+}
